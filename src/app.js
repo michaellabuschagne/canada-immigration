a90d8802101837baa6c64350e97392306a318488
Cat_wiCover <cat@wicover.co>
2020-10-04 22:24:03 +0200

    Add CAD exchange rate API call

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // https://www.canada.ca/content/dam/ircc/documents/json/data-ptime-non-country-en.json
 const canadaProcessingTimesUrl = 'https://www.canada.ca/content/dam/ircc/documents/json/data-ptime-en.json';
+const cadExchangeUrl = 'https://api.exchangeratesapi.io/latest?base=CAD&symbols=ZAR'
 
 const axios = require('axios').default;
 
@@ -10,6 +11,18 @@ axios.get(canadaProcessingTimesUrl)
             console.log('Last Updated:', lastupdated);
             console.log('Work permit (from outside Canada) - South Africa:', ZA);
             
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+axios.get(cadExchangeUrl)
+    .then(response => {
+        if (response.data) {           
+            // {"rates":{"ZAR":12.4336345575},"base":"CAD","date":"2020-10-02"}
+            const { data : { rates : { ZAR : zarRate }, date } } = response;
+            console.log(`CAD\\ZAR exchange rate R${zarRate}`);     
         }
     })
     .catch(error => {
